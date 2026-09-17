b1e33bfdbcdde1a5d55a473c293c4ed323f28197
Soraxsnac <r.sanchez.gonzalez@ugto.mx>
2026-09-16 19:04:03 -0600

    MÓDULO-01

---
+++ CheGato/CheGato.Api/Controllers/ProductosController.cs
@@ -0,0 +1,36 @@
+﻿using CheGato.Api.Data;
+using CheGato.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheGato.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly CheGatoContext _context;
+
+        public ProductosController(CheGatoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Productos (Este método devolverá todo el catálogo)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        // POST: api/Productos (Este método sirve para crear nuevos productos)
+        [HttpPost]
+        public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductos), new { id = producto.Id }, producto);
+        }
+    }
+}
